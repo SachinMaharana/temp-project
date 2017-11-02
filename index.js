29dc3c19e41bc156590f0669aa7e1ba82d707b60
Sachin <sachin.nicky.com>
2017-11-02 22:07:45 +0530

    authetication alomst donr

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-
 const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
 
-const app = express();
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientId,
-      clientSecret: keys.googleSecretId,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accc", accessToken);
-      console.log("refresh", refreshToken);
-      console.log("profile", profile);
-    }
-  )
-);
+mongoose.Promise = Promise;
 
-app.get("/", (req, res) => {
-  res.send({ hi: "Sachin!" });
+mongoose.connect(keys.mongoURI, {
+  useMongoClient: true
 });
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
   })
 );
 
-app.get("/auth/google/callback", passport.authenticate("google"));
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Good to go!");
+  console.log("Good to Go!");
 });
